9830832a5e68ba89d68f67592ff18e714a128fe2
Roman Prajapati <rmnprjpt@gmail.com>
2016-01-08 11:27:53 +0530

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,15 +1,15 @@
 data_full <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
                       nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
-
+#analysis of only 2 days
 data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 rm(data_full)
 
 datetime <- paste(as.Date(data$Date), data$Time)
 data$Datetime <- as.POSIXct(datetime)
-
+#creates histogram where Global Active Power in X-Axis and Frequency in Y-AXIS
 hist(data$Global_active_power, main="Global Active Power", 
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
-
+#create plot1.png in working directory with height and weight specified.
 dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()
